ffd41d34d0ab4000fa36c81b7781076fa786f73e
ameer suhail <suhail@ameers-MacBook-Air.local>
2026-02-11 21:35:50 +0530

    dsd

--- app/components/rusable/Redirect.tsx
+++ app/components/rusable/Redirect.tsx
@@ -3,12 +3,12 @@ import { useRouter } from 'next/navigation';
 import React from 'react'
 
 import { MdKeyboardArrowLeft } from "react-icons/md";
-const Redirect = ({ path, color }: { path: string, color: string }) => {
+const Redirect = ({ path }: { path: string }) => {
     const router = useRouter()
     return (
 
 
-        <div onClick={() => { router.push(path) }} className={`p-1 rounded-2xl ${color}`}>
+        <div onClick={() => { router.push(path) }} className={`p-1 rounded-2xl bg-gray-600`}>
             <MdKeyboardArrowLeft className='text-white text-xl' />
         </div>
     )
--- app/viewservice/[type]/[id]/page.tsx
+++ app/viewservice/[type]/[id]/page.tsx
@@ -23,7 +23,7 @@ const page = async ({ params }: { params: { type: string, id: string } }) => {
     return (
         <div className='pb-20 bg-gray-950'>
             <div className={`${type === ServiceType.TURF ? "bg-gray-900" : "bg-gray-50"} flex justify-between  p-2`}>
-                <Redirect color={`${type == ServiceType.AUTOSERVICES ? "bg-gray-100" : type == ServiceType.FOODDELIVERY ? "bg-gray-100" : type == ServiceType.TURF ? "bg-gray-900" : ""}`} path={`/services/${type}`} />
+                <Redirect path={`/services/${type}`} />
             </div>
             <Dynamiccard />
         </div>
